35737d6af48d2d56b9860b9e204d277e69a814bf
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-01-31 22:30:39 +0530

    Day -08 add

---
+++ Day-08.js
@@ -0,0 +1,32 @@
+a = 10;
+a=20
+
+a="Hello Sir"
+
+var b = "var b"
+b = 34
+var b = "2nd var b"
+var b = "3rd var b"
+b = "4th b"
+b = 34;
+
+let d = "Hiii"
+d = 55
+d = 67 
+
+const c = 100
+
+console.log(a)
+console.log(b)
+console.log(d)
+console.log(c)
+
+
+x = 20
+y = "Byee"
+z = true
+s = 84.80
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+console.log(typeof s)
