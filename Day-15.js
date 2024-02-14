ce5ab216d7b361f4c93e4487981b1b28933aee9a
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-02-14 20:27:53 +0530

    update

--- Day-15.js
+++ Day-15.js
@@ -1,2 +1,15 @@
-a="hello"
-console.log(a);
+function login(){
+    var h3 = document.getElementById("msg");
+}
+
+function checkmail(){
+    var e = document.getElementById("uname").value
+
+    var emailRegex = /\S+@\S+\.\S+/.test(e);
+
+    if (emailRegex) {
+        document.getElementById("echeck").innerHTML="Email is correct"
+    } else {
+        document.getElementById("echeck").innerHTML="Email is Incorrect"
+    }
+}
