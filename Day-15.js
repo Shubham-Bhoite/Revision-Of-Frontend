ea2e0e7caefceafbbde1208448830c381611e79d
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-02-15 09:36:08 +0530

    Day-15 update

--- Day-15.js
+++ Day-15.js
@@ -1,5 +1,6 @@
+
 function login(){
-    var h3 = document.getElementById("msg");
+    window.open("login.html")
 }
 
 function checkmail(){
@@ -9,7 +10,48 @@ function checkmail(){
 
     if (emailRegex) {
         document.getElementById("echeck").innerHTML="Email is correct"
+        return true;
+
     } else {
         document.getElementById("echeck").innerHTML="Email is Incorrect"
+        return false;
+    }
+}
+
+function checkpass(){
+    var p = document.getElementById("pass").value
+
+    var passRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}/.test(p);
+
+    if (passRegex) {
+        document.getElementById("pmsg").innerHTML="Password is strong"
+        return true;
+
+    } else {
+        document.getElementById("pmsg").innerHTML="Password is weak"
+        return false;
+    }
+}
+
+function checkmob(){
+    var m = document.getElementById("mob").value
+
+    var mobRegex = /^\d{10}$/.test(mob);
+
+    if (mobRegex) {
+        document.getElementById("mmsg").innerHTML="Mobile number is valid"
+        return true;
+
+    } else {
+        document.getElementById("mmsg").innerHTML="Mobile number Invalid"
+        return false;
+    }
+}
+
+function login(){
+    if (checkmail() && checkpass() && checkmob()){
+        window.open("login.html")
+    }else{
+        document.getElementById("msg").innerHTML = "Invalid Id or Password"
     }
 }
