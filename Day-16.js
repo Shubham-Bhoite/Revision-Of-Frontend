7dc00e8c33f31f6119f5786bd47ee274e7b49896
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-02-17 10:27:59 +0530

    Day-16 update

--- Day-16.js
+++ Day-16.js
@@ -3,6 +3,6 @@ let c = 0
 var images=["Images/download (1).jpg", "Images/download (2).jpg", "Images/download (3).jpg"]
 
 function changeimg(){
-    c = (c+1)
+    c = (c+1) % images.length;
     document.getElementById("changeimage").innerHTML= "<img src='"+images[c]+"'>"
 }
